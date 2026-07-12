3f6842f2b5ef4ce9f28b344ff0f0fda4d8f09ca3
NaeemAdnan <naeem.cse.nwu@gmail.com>
2026-07-12 23:45:42 +0600

    main file a veriable add

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Naeem Adnan")
+// 	fmt.Println("Hello Sundarban")
+// }
+
+
 package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello, World!")
+func main(){
+	fmt.Println("Naeem valo ache")
+	fmt.Println("She biri khayna")
+	fmt.Println("KObe valo job pabo")
+	fmt.Println("Amar shonar Bangla")
 }
