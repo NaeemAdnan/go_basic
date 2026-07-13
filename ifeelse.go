a91f8d4c42a3cd8136d47ebc6ba969c49bf9b471
NaeemAdnan <naeem.cse.nwu@gmail.com>
2026-07-13 23:32:40 +0600

    emni

--- ifeelse.go
+++ ifeelse.go
@@ -7,7 +7,7 @@ func main() {
 	age := 20
 
 	if age > 20 {
-		fmt.Println("You are eligible for marriage")
+		fmt.Println("You are eligible for marriage. lalala")
 	} else  if age == 20{
 		fmt.Println("You are not eligible for marriage, but you can start looking for a partner")
 	} else {
